a9bdaf97b04da52840bedf2d748509c9415bf893
Ozymandias-II <jordy_alexander_vargas@outlook.com>
2022-07-13 19:28:16 -0500

    Configuracion inicial

---
+++ src/main/java/ec/edu/espe/vargascorebancario/VargasCoreBancarioApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.vargascorebancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VargasCoreBancarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VargasCoreBancarioApplication.class, args);
+    }
+
+}
